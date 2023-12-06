c51bf97397ebf2ea21e6b5b913048782877189fc
Wook <chosh0206@naver.com>
2023-12-06 23:35:05 +0900

    커밋

--- models/User.js
+++ models/User.js
@@ -31,10 +31,6 @@ const userSchema = new Schema({
     required: [true, '전화번호를 입력해주세요'],
     minLength: [13, '유효한 전화번호를 입력해주세요'],
   },
-  clearStage: {
-    type: String,
-    default: 0,
-  },
   selectscore1: {
     type: Number,
     default: 0,
